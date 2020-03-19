73e1060e13f6d59ba03a7d1df7d2324c017a94c1
Preslav Rachev <rachev.preslav@gmail.com>
2020-03-19 08:56:59 +0100

    Extend the main loop to provide slice support

--- dig.go
+++ dig.go
@@ -2,9 +2,25 @@ package dig
 
 import (
 	"errors"
+	"strconv"
 )
 
+type enumerable interface {
+	getAt(key string) interface{}
+	setAt(key string, value interface{})
+}
+
 type dict map[string]interface{}
+
+func (d dict) getAt(key string) interface{} {
+	return d[key]
+}
+
+func (d dict) setAt(key string, value interface{}) {
+	d[key] = value
+}
+
+type slice []interface{}
 type cache map[string]func(...interface{}) interface{}
 type getterSetterFunc func(i ...interface{}) interface{}
 
@@ -16,36 +32,11 @@ type mmap struct {
 func NewMap(source dict) *mmap {
 	cache := make(cache)
 
-	recurse(source, cache, "")
+	recurseDict(source, cache, "")
 
 	return &mmap{
 		source: source,
 		cache:  cache,
-	}
-}
-
-func recurse(m dict, cache cache, pathPrefix string) {
-	for key, value := range m {
-
-		path := key
-		if pathPrefix != "" {
-			path = pathPrefix + "." + key
-		}
-
-		switch value.(type) {
-		case map[string]interface{}:
-			recurse(value.(map[string]interface{}), cache, path)
-		case string, int, int32, int64, float32, float64:
-			cache[path] = func(m dict, key string) getterSetterFunc {
-				return func(i ...interface{}) interface{} {
-					if i != nil {
-						m[key] = i[0]
-					}
-
-					return m[key]
-				}
-			}(m, key)
-		}
 	}
 }
 
@@ -66,6 +57,58 @@ func (m *mmap) SetValue(path string, value interface{}) error {
 
 	setterFunc(value)
 	return nil
+}
+
+func (m *mmap) PropertyPaths() []string {
+	var pp []string
+	for key := range m.cache {
+		pp = append(pp, key)
+	}
+
+	return pp
+}
+
+func recurseDict(m dict, cache cache, pathPrefix string) {
+	for key, value := range m {
+
+		path := key
+		if pathPrefix != "" {
+			path = pathPrefix + "." + key
+		}
+
+		process(value, cache, path, m, key)
+	}
+}
+
+func process(value interface{}, cache cache, path string, m enumerable, key string) {
+	switch value.(type) {
+	case map[string]interface{}:
+		recurseDict(value.(map[string]interface{}), cache, path)
+	case []interface{}:
+		recurseSlice(value.([]interface{}), cache, path)
+	case string, int, int32, int64, float32, float64:
+		cache[path] = func(m enumerable, key string) getterSetterFunc {
+			return func(i ...interface{}) interface{} {
+				if i != nil {
+					m.setAt(key, i[0])
+				}
+
+				return m.getAt(key)
+			}
+		}(m, key)
+	}
+}
+
+func recurseSlice(m slice, cache cache, pathPrefix string) {
+	for idx, value := range m {
+		strIdx := strconv.Itoa(idx)
+		path := strIdx
+		if pathPrefix != "" {
+			path = pathPrefix + "." + strIdx
+		}
+
+		process(value, cache, path, nil, strIdx)
+	}
 }
 
 func (m *mmap) Source() dict {
